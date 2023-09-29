4db4315183d3074bf03a412a18bab35ffe382729
Prasses Khadka <khadkaprasses@gmail.com>
2023-09-29 13:51:37 -0400

    basic test case added

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../src/app/page";
-<<<<<<< HEAD
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
@@ -13,17 +12,5 @@ describe("Home", () => {
 
     // ASSERT
     expect(word).toBeInTheDocument();
-=======
-
-describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />); //ARRANGE
-
-    const heading = screen.getByRole("heading", {
-      name: /Next.js Testing\.js!/i,
-    });
-
-    expect(heading).toBeInTheDocument();
->>>>>>> 7c81a88 (Setup complete and first test passed)
   });
 });
